dfb86bd6508e79d05a303434866dceb88afdb822
FarhanIsrakYen <farhan.yen@northsouth.edu>
2024-05-25 21:17:19 +0600

    added footer and project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function sendEmail() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Message sent successfully"
+      });
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        message: document.getElementById('message').value,
+    }
+
+    emailjs.send("service_y611hm8", "template_ahg5sek", params);
+}
